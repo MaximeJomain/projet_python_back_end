3dd221fce177e7326a96a1b03e2467f8ec1fc328
Maxime_Luchini <72389960+MaximeLuchini@users.noreply.github.com>
2021-10-21 11:59:41 +0200

    function display_playlist done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from functions.connect_db import connect_db
 from functions.create_playlist import create_playlist
+from functions.display_playlists import display_playlist
 
 import pymongo
 
@@ -8,3 +9,5 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     create_playlist(connect_db(), "kebab")
+    display_playlist(connect_db())
+
