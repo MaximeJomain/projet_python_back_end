71b4335bd378fdd92a4e723f6c0b20f8806d1f1e
Maxime_Luchini <72389960+MaximeLuchini@users.noreply.github.com>
2021-10-21 09:52:54 +0200

    import files in main

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 from flask import Flask
+from Functions.connect_db import connect_db
+
 import pymongo
 
 app = Flask(__name__)
 
+if __name__ == "__main__" :
 
-def main():
-    client = pymongo.MongoClient(
-        "mongodb+srv://coding:bad123@cluster0.jmgfz.mongodb.net/spotify_db?retryWrites=true&w=majority")
-    db = client.test
-    collection = db.collection0
-    for i in range(1, 11):
-        playlist = {
-            "_id": i,
-            "playlist_name": f"Playlist_name {i}",
-        }
-        playlist_id = collection.insert_one(playlist).inserted_id
-        print(f"Inserted {playlist_id}: {playlist}")
-
-
-if __name__ == '__main__':
-    main()
+    connect_db()
