d06d0b41ad48a26604fcc5970b0c5c0cf125cabd
killian <72389975+buldOo@users.noreply.github.com>
2021-10-21 17:24:20 +0200

    add delete_playlist function

--- functions/delete_playlist.py
+++ functions/delete_playlist.py
@@ -1,5 +1,20 @@
-def delete_playlist(db):
-    database = db
-    collection = database.playlists
-    playlist = {"name": "teubidélice"}
-    collection.delete_one(playlist)
+from markupsafe import escape
+from flask import Blueprint, request
+from functions.connect_db import connect_db
+
+app_delete_playlist = Blueprint('app_delete_playlist', __name__)
+
+
+@app_delete_playlist.route("/delete/<name>", methods=['DELETE'])
+def delete_playlist(name):
+    db = connect_db()
+    playlist_list = db.playlists.find({})
+    if request.method == "DELETE":
+        name = escape(name)
+        collection = db.playlists
+        for playlist in playlist_list:
+            print(playlist["name"])
+            if playlist["name"] == name:
+                return f"delete {name}"
+                collection.delete_one({'name': name})
+        return "this playlist doesn't exist"
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from functions.create_playlist import app_create_playlist
 from functions.display_playlist import app_display_playlist
+from functions.delete_playlist import app_delete_playlist
 
 app = Flask(__name__)
 app.register_blueprint(app_create_playlist)
+app.register_blueprint(app_delete_playlist)
 app.register_blueprint(app_display_playlist)
 
 
