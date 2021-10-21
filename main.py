938e90db31bbab138e00e0c976eab80bbce89111
Maxime_Luchini <72389960+MaximeLuchini@users.noreply.github.com>
2021-10-21 15:24:43 +0200

    display_playlist finished

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from functions.create_playlist import app_create_playlist
+from functions.display_playlist import app_display_playlist
 
 app = Flask(__name__)
 app.register_blueprint(app_create_playlist)
+app.register_blueprint(app_display_playlist)
 
 
 @app.route('/')
