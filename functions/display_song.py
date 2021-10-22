421576f7e47cc810a1c1aa51159d8e32a20a0bfb
killian <72389975+buldOo@users.noreply.github.com>
2021-10-22 11:40:19 +0200

    display_song.py

--- functions/display_song.py
+++ functions/display_song.py
@@ -7,15 +7,20 @@ app_display_song = Blueprint('app_display_song', __name__)
 
 @app_display_song.route('/song/<id>', methods=['GET'])
 def display_song(id):
+    """ this function return a choosed songs
+
+    :param id: the id of the song you want to display
+    :return: all the information of the choosed songs
+    """
     db = connect_db()
     song_list = db.songs.find({})
     if request.method == 'GET':
         id = str(escape(id))
+        result = []
         for song in song_list:
             if str(song["_id"]) == id:
-                print(song["_id"])
-                print(song["author"])
-                print(song["title"])
-                print(song["duration"])
+                result.append(f"id: {song['_id']} / auteur: {song['author']} / titre: {song['title']} / durée: {song['duration']}")
+                return f"{result}"
+        return "this song doesn't exist"
 
 
