a5fb820bd70a757bd24af13687e4877df5645d84
Maxime_Luchini <72389960+MaximeLuchini@users.noreply.github.com>
2021-10-22 09:35:25 +0200

    function update_playlist

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from functions.create_playlist import app_create_playlist
 from functions.display_playlist import app_display_playlist
+from functions.update_playlist import app_update_playlist
 
 app = Flask(__name__)
 app.register_blueprint(app_create_playlist)
 app.register_blueprint(app_display_playlist)
+app.register_blueprint(app_update_playlist)
 
 
 @app.route('/')
